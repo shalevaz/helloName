224db496b37a9a7fbbd77e3ffa57707990881483
shalevaz <shalevazoulay27@gmail.com>
2020-08-20 13:43:20 +0300

    I changed the code

--- hello.py
+++ hello.py
@@ -1,3 +1,8 @@
-name = input("What is your name?")
-print("Hello "+name)
+name = input("Insert your name ")
 
+age = input("Insert your age ")
+age = int(age)
+if age > 18:
+    print ("Hello "+name+" you are allow to continue")
+else:
+    print("Hello "+name+" you are too little please try again in a few year")
